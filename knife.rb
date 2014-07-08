1baeef499e00afb5b6ed8a705e2ef25f670e8ac9
Braulio Bhavamitra <braulio@eita.org.br>
2014-07-08 11:29:36 -0300

    Update knife.rb from example

--- knife.rb
+++ knife.rb
@@ -1,17 +1,22 @@
-base_path = "#{File.dirname __FILE__}"
-node_name 'brauliobo'
+# based on http://docs.opscode.com/essentials_repository.html
+current_dir = File.dirname __FILE__
+user = ENV['OPSCODE_USER'] || ENV['USER']
+node_name user
+
+client_key "#{ENV['HOME']}/.chef/#{user}.pem"
+validation_client_name "#{ENV['ORGNAME']}-validator"
+validation_key "#{ENV['HOME']}/.chef/#{ENV['ORGNAME']}-validator.pem"
+
+chef_server_url "https://api.opscode.com/organizations/#{ENV['ORGNAME']}"
+
+cookbook_path ["#{current_dir}/cookbooks"]
+cookbook_copyright "EITA"
+cookbook_license "apachev2"
+cookbook_email "coletivo@eita.org.br"
+
 #
 #log_level :info
 #log_location STDOUT
-#
-client_key "#{base_path}/brauliobo.pem"
-#validation_client_name 'chef-validator'
-#validation_key '/etc/chef-server/chef-validator.pem'
-#
-#chef_server_url 'https://localhost:443'
 
-cookbook_path [
-  "#{base_path}/cookbooks",
-]
+syntax_check_cache_path "#{ENV['HOME']}/.chef/syntax_check_cache"
 
-syntax_check_cache_path "#{base_path}/syntax_check_cache"
